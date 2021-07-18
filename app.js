f939ea78805594e318ede479d43535b777a391b9
SebaD95 <sebadiaz95@hotmail.com>
2021-07-18 18:04:19 -0300

    wh

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post("/procesar-pago", function (req, res) {
       installments: 6,
       default_installments: 6
     },
-    notification_url:"https://sebad95-mp-commerce-nodejs.herokuapp.com/notification-webhook",
+    notification_url:"https://hookb.in/LggNrjpjOMh18Vqq8xBE",
     statement_descriptor: "Tienda e-commerce",
     external_reference: "sebadiaz95@hotmail.com"
   };
